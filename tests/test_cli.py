9f29b62c364718e511b9884979611b66cfdddecf
Haseeb Majid <hello@haseebmajid.dev>
2021-03-14 18:04:56 +0000

    work

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import filecmp
 import time
 
 import pytest
@@ -48,24 +49,6 @@ def test_fail_args(runner, args, exit_code):
             [
                 [
                     {
-                        "id": "123797",
-                        "body_markdown": "---\nchecksum: 7eb95ce783e8896686c88c739cd30ac3\ncover_image: https://cdn-images-1.medium.com/max/1024/0*zoKBYDQXHpqWod1E.png\npublished: false\ntags: ci,react-native,gitlab\ntitle: A Test Message\n---\n\n> Before we dive straight into the CI file itself, let’s do a quicker refresher on some basic concepts. with CI/CD, Git and Gitlab CI.\n\nContinuous Integration (CI), is typically defined as making sure all code being integrated into a codebase works. It usually involves running a set of jobs referred to as a CI pipeline. Some jobs we may run include lintinusuasia tool such as Travis, Circle or even Gitlab.\n\n![a](images/a.png)\n![b](images/b.jpg)",
-                        "title": "A Test Message",
-                    },
-                    {
-                        "id": "78979",
-                        "body_markdown": "---\nchecksum: dabaaec68730e589699a6006b4a73ce2\ncover_image: https://dev-to-uploads.s3.amazonaws.com/i/w00r4rpmfpjqb8wgygxu.jpg\nlicense: public-domain\ntags:\n- React Native\n- CI\n- GitLab\n- Automation\n- Android\ntitle: Auto Publish React Native App to Android Play Store using GitLab CI\n---\n\nIn this article, I will show you how can automate the publishing of your AAB/APK to the `Google Play Console`.\nWe will be using the [Gradle Play Publisher](https://github.com/Triple-T/gradle-play-publisher) (GPP) plugin to do\nautomate this process for us. Using this plugin we cannot only automate the publishing and release of our app,\nwe can also update the release notes, store listing (including photos) all from GitLab CI. \n\n**Note:** In this article I will assume that you are using Linux and React Native version >= 0.60.\n\n![c](c.jpg)\n![c](c.jpg)\n![c](c.jpg)\n\n---------------------------------------------------------------------------------------------------",
-                        "title": "Auto Publish React Native App to Android Play Store using GitLab CI",
-                    },
-                ],
-                [],
-            ],
-            ["-k", "AKEY", "-m", "tests/data/another.md", "-o", "tests/data/tmp/"],
-        ),
-        (
-            [
-                [
-                    {
                         "id": "78979",
                         "body_markdown": "---\nchecksum: dabaaec68730e589699a6006b4a73ce2\ncover_image: https://dev-to-uploads.s3.amazonaws.com/i/w00r4rpmfpjqb8wgygxu.jpg\nlicense: public-domain\ntags:\n- React Native\n- CI\n- GitLab\n- Automation\n- Android\ntitle: Auto Publish React Native App to Android Play Store using GitLab CI\n---\n\nIn this article, I will show you how can automate the publishing of your AAB/APK to the `Google Play Console`.\nWe will be using the [Gradle Play Publisher](https://github.com/Triple-T/gradle-play-publisher) (GPP) plugin to do\nautomate this process for us. Using this plugin we cannot only automate the publishing and release of our app,\nwe can also update the release notes, store listing (including photos) all from GitLab CI. \n\n**Note:** In this article I will assume that you are using Linux and React Native version >= 0.60.\n\n![c](c.jpg)\n![c](c.jpg)\n![c](c.jpg)\n\n---------------------------------------------------------------------------------------------------",
                         "title": "Auto Publish React Native App to Android Play Store using GitLab CI",
@@ -91,19 +74,24 @@ def test_fail_args(runner, args, exit_code):
     ],
 )
 def test_success(mocker, runner, devto_articles, args):
-    get_mock = mocker.Mock(status_code=200)
-    get_mock.json.side_effect = devto_articles
-    mocker.patch("requests.get", return_value=get_mock)
-
-    create_mock = mocker.Mock(status_code=201)
-    mocker.patch("requests.post", return_value=create_mock)
-    create_mock.json.return_value = {"data": {"link": "https://imgur.com/123456"}, "url": "random_url.com"}
-    mocker.patch("requests.put", return_value=create_mock)
-    result = runner.invoke(cli, args)
+    result = run_cli(mocker, runner, devto_articles, args)
     assert result.exit_code == 0
 
-    if "-o" in args:
-        pass
+
+@pytest.mark.parametrize(
+    "args, output, expected_file",
+    [
+        (
+            ["-k", "AKEY", "-m", "tests/data/another.md", "-o", "tests/data/tmp/"],
+            "tests/data/tmp/Auto-Publish-React-Native-App-to-Android-Play-Store-using-GitLab-CI.md",
+            "tests/data/expected/another.md",
+        ),
+    ],
+)
+def test_output_file(mocker, runner, args, output, expected_file):
+    result = run_cli(mocker, runner, [""], args)
+    assert result.exit_code == 0
+    assert filecmp.cmp(output, expected_file)
 
 
 def test_dev_to_api_auth_failure_get_articles(mocker, runner):
@@ -155,3 +143,15 @@ def test_dev_to_api_connection_failure_upload_articles(mocker, runner, exception
 def test_rate_limiting(time_wait):
     articled_uploaded, start = check_if_we_need_to_rate_limit(10, time.time() - time_wait)
     assert articled_uploaded == 0 and type(start) == float
+
+
+def run_cli(mocker, runner, devto_articles, args):
+    get_mock = mocker.Mock(status_code=200)
+    get_mock.json.side_effect = devto_articles
+    mocker.patch("requests.get", return_value=get_mock)
+    create_mock = mocker.Mock(status_code=201)
+    mocker.patch("requests.post", return_value=create_mock)
+    create_mock.json.return_value = {"data": {"link": "https://imgur.com/123456"}, "url": "random_url.com"}
+    mocker.patch("requests.put", return_value=create_mock)
+    result = runner.invoke(cli, args)
+    return result
